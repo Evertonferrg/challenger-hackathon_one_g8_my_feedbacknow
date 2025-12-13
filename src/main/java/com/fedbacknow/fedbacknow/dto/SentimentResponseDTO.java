210ebd021beb32ad9b19aa7972a745d133108947
Everton Ferreira Guedes <evertonferrg@gmail.com>
2025-12-13 20:02:16 -0300

    Configurando Postgres

--- src/main/java/com/fedbacknow/fedbacknow/dto/SentimentResponseDTO.java
+++ src/main/java/com/fedbacknow/fedbacknow/dto/SentimentResponseDTO.java
@@ -2,5 +2,8 @@ package com.fedbacknow.fedbacknow.dto;
 
 import com.fedbacknow.fedbacknow.domain.SentimentType;
 
-public record SentimentResponseDTO(String text, String productName, SentimentType sentiment) {
+import java.time.LocalDateTime;
+
+public record SentimentResponseDTO(Long id, String text, String productName, SentimentType sentiment,
+                                   LocalDateTime createdAt) {
 }
